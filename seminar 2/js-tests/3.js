95909c9380ef8df4e7b2c6a89c741a23790b36a6
Catalin0910 <catalincebuc99@gmail.com>
2020-10-07 13:19:56 +0300

    actualizare seminar2

---
+++ seminar 2/js-tests/3.js
@@ -0,0 +1,21 @@
+let b = false;
+console.log(!"");
+console.log(!0);
+
+let s = "14.3";
+console.log(parseFloat(s));
+console.log(parseInt(s));
+console.log(parseFloat("abcde"));
+console.log(!NaN);
+console.log(!null);
+
+let c;
+console.log(c);
+console.log(typeof c);
+//console.log(d);
+
+let o = {
+  name: "catalin",
+  age: 21,
+};
+console.log(o);
