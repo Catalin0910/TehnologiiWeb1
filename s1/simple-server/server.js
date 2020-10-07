1ae769437cb381f76e253dc8a2983950be6c59ca
Catalin0910 <catalincebuc99@gmail.com>
2020-10-07 13:05:44 +0300

    seminar2

--- s1/simple-server/server.js
+++ s1/simple-server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/ping", (req, res) => {
-  res.send("pong");
+  res.send("pong123");
 });
 
 app.listen(8080);
---
+++ seminar 2/js-tests/2.js
@@ -0,0 +1,15 @@
+let n = 5;
+console.log(typeof n);
+
+let s = 4;
+
+console.log(typeof s);
+
+s = "5";
+
+console.log(typeof s);
+
+console.log(2 ** 4); //operator de exponentiere
+console.log(n == s);
+console.log(n + s);
+console.log(n === s);
